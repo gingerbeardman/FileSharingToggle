2d2b222679b80d611ae13bc94bdbdae310e9ae6f
Matt Sephton <matt.sephton@gmail.com>
2024-08-28 14:07:33 +0100

    Initial Commit

---
+++ FileSharingToggle/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FileSharingToggle
+//
+//  Created by Matt Sephton on 28/08/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
